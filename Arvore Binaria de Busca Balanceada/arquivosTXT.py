5ada5f3e003383043fbe7beb19ff603e2a2ce733
alanpablo33 <alanpablo137@gmail.com>
2022-11-22 13:32:43 -0300

    22/11/2022

--- Arvore Binaria de Busca Balanceada/arquivosTXT.py
+++ Arvore Binaria de Busca Balanceada/arquivosTXT.py
@@ -3,39 +3,32 @@ from palavras import palavras
 
 dados1 = []
 
-def selecionar_palavras(): # função 
-    palavra = random.choice(palavras) # variavel que ira escolher uma palavra aleatoria do nosso modulo importado
-    return palavra.upper() # retornara a palavra escolida com letras maiusculas
+def dados_txt(): # add no TXT
+    
+    cont = 10 # contador que vai dar as chaves sequencialmente para cada linha
+    for c in range(cont):
+        chave = '{:01d}' .format(c) 
 
-def selecionar_numeros(): #colocar numero aleatorio
-    lista = list(range(1,100))
-    for i in lista:
-        n = random.randint(1,10)
-    return n
-
-def dados_txt(valor_inteiro,char): # add no TXT
-    cont = 10
-    for item in range(cont):
-        file = '{:01d}' .format(item) # contar
-        dados1.append({
-            'chave': file,
-            'valor_inteiro': valor_inteiro,
-            'char_1000':char 
-        })
+        lista = list(range(1)) # Numeros de 1 a 10.000, que ira entrar aleatoriamente 
+        for i in lista:
+            n = random.randint(1,10000)
         
-    with open('DADOS.txt', 'a') as arquivo:
+        palavra = random.choice(palavras) # variavel que ira escolher uma palavra aleatoria do nosso modulo importado
+        
+        #lista a ser preenchida com CHAVE,INTEIRO e CHAR
+        dados1.append({
+            'chave': chave,
+            'valor_inteiro': n,
+            'char_1000':palavra 
+         })
+        
+    with open('DADOS.txt', 'a') as arquivo: # ENVIANDO TUDO PARA O ARQUIVO TXT
         for l1 in dados1:
             arquivo.write(f'CHAVE: {l1["chave"]} - VALOR INTEIRO: {l1["valor_inteiro"]}- CHAR: {l1["char_1000"]}')
             arquivo.write('\n')
 
-def iniciar():
-    palavra = selecionar_palavras()
-    numero = selecionar_numeros()
-    dados_txt(numero,palavra)
+def iniciar():# RODAR CODIGO
+
+    dados_txt()
 
 iniciar()
-
-
-
-
-  
--- Arvore Binaria de Busca Balanceada/numeros.py
+++ Arvore Binaria de Busca Balanceada/numeros.py
@@ -1,2 +1,8 @@
-x = list(range(1,10000))
-print(x)
+#x = list(range(1,10000))
+#print(x)
+
+def contar_chave():
+    
+    for c in range(10):
+        file = '{:01d}' .format(c) # contar
+    return file
